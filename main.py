5267de8b76771559f640dd84456f362c46a5c44a
Tristan Pitts <trp42@pitt.edu>
2021-02-17 10:22:35 -0500

    Clean up image processing

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-from PIL import Image, ImageFilter
+from PIL import Image, ImageOps, ImageEnhance, ImageFilter, ImageChops
 
 def main():
     
     image = Image.open('test.jpg')
+    image = image.convert("L")
+    image = ImageOps.colorize(image, 'black', 'white', mid=None, blackpoint=0, whitepoint=255, midpoint = 127)
     image = image.filter(ImageFilter.FIND_EDGES)
+    image = ImageChops.invert(image)
     image.save('imagef.jpg')
 
 
