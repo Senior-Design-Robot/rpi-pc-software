69ce2dae8b0a35550e8b8f00c2278425421660a3
Tristan Pitts <tristanpitts1@gmail.com>
2021-03-11 14:47:49 -0500

    Path planning for demo 2

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import serial
 import time
 import numpy
 import cv2
+import math
 
 def main():
 
@@ -16,6 +17,35 @@ def main():
     cv2.imshow('Contours', canny)
     cv2.waitKey(0)
     cv2.imwrite('out.jpg', canny)
+
+    averages = []
+    moves = []
+
+    for contour in contours:
+        averages.append(cv2.mean(contour))
+
+    currentMove = 0
+    for point in averages:
+        distances = []
+        for point2 in averages:
+            if point != point2:
+                distances.append(distance(point2, point))
+        while numpy.argmin(distances) in moves:
+            distances.remove(distances[numpy.argmin(distances)])
+        moves.append(numpy.argmin(distances))
+
+    order = []
+    order.append(0)
+    currentContour=0
+
+    for m in moves:
+        order.append(moves[currentContour])
+        currentContour = moves[currentContour]
+
+    print(order)
+
+def distance(p2, p1):
+    return math.sqrt((p2[0]-p1[0])**2 + (p2[1]-p1[1])**2)
 
 if __name__ == "__main__":
     main()
