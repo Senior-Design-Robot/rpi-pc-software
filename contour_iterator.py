a4caecdc8f4429a5ab8db5c96057642343c061bc
Katy Fox <katycat5e@gmail.com>
2021-04-05 15:27:11 -0400

    add contour/path iterator

---
+++ contour_iterator.py
@@ -0,0 +1,87 @@
+import math
+from typing import List, Tuple
+
+import cv2
+
+from esp_wifi import PathElementType
+
+
+def distance(p2, p1):
+    return math.sqrt((p2[0] - p1[0]) ** 2 + (p2[1] - p1[1]) ** 2)
+
+
+class ContourIterator:
+    def __init__(self, contours):
+        # moves contains the next contour to move to from the current index
+        self.contours = contours
+        self.contour_idx = 0
+        self.point_in_contour = 0
+        self.moves = []
+
+        averages = [cv2.mean(contour) for contour in contours]
+
+        # initially, all are unvisited except the start point
+        unvisited_idx = list(range(1, len(averages)))
+        current_idx = 0
+
+        while len(unvisited_idx) > 0:
+            cur_point = averages[current_idx]
+
+            # find the next point that is closest to the current
+            min_dist = float('inf')
+            min_idx = -1
+
+            for dest_idx in unvisited_idx:
+                tgt_distance = distance(averages[dest_idx], cur_point)
+
+                if tgt_distance < min_dist:
+                    min_idx = dest_idx
+                    min_dist = tgt_distance
+
+            current_idx = min_idx
+            unvisited_idx.remove(min_idx)
+            self.moves.append(min_idx)
+
+    def reset(self):
+        self.contour_idx = 0
+        self.point_in_contour = 0
+
+    @property
+    def current_contour(self):
+        return self.contours[self.contour_idx]
+
+    def __has_next_contour(self) -> bool:
+        # if there is a next move defined for current contour
+        return self.contour_idx < len(self.moves)
+
+    def __move_next_contour(self):
+        self.contour_idx = self.moves[self.contour_idx]
+        self.point_in_contour = 0
+
+    def __has_next_point(self) -> bool:
+        return (self.point_in_contour < len(self.current_contour)) or self.__has_next_contour()
+
+    def __dequeue_next_point(self) -> Tuple[PathElementType, float, float]:
+        if self.point_in_contour < len(self.current_contour):
+            # normal point
+            point = self.current_contour[self.point_in_contour]
+            self.point_in_contour += 1
+
+            elem_type = PathElementType.PATH_MOVE if (self.point_in_contour > 0) else PathElementType.PATH_PEN_DOWN
+            return elem_type, point[0], point[1]
+
+        else:
+            # break to next contour
+            point = self.current_contour[-1]
+            self.__move_next_contour()
+            return PathElementType.PATH_PEN_UP, point[0], point[1]
+
+    def get_points(self, max_count: int) -> List[Tuple[PathElementType, float, float]]:
+        count = 0
+        pt_list = []
+
+        while (count < max_count) and self.__has_next_point():
+            next_point = self.__dequeue_next_point()
+            pt_list.append(next_point)
+
+        return pt_list
--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-from PIL import Image, ImageOps, ImageEnhance, ImageFilter, ImageChops
-import serial
-import time
-import numpy
 import cv2
-import math
+
+from contour_iterator import ContourIterator
 
 def main():
-
     temp = cv2.imread('teest2.jpg', cv2.IMREAD_UNCHANGED)
     blur = cv2.blur(temp, (5, 5))
     canny = cv2.Canny(blur, 30, 150)
@@ -14,37 +10,10 @@ def main():
     cv2.drawContours(canny, contours, -1, 255, 2)
     canny = 255 - canny
 
-
     cv2.imwrite('out.jpg', canny)
 
-    averages = []
-    moves = []
-
-    for contour in contours:
-        averages.append(cv2.mean(contour))
-
-    currentMove = 0
-    for point in averages:
-        distances = []
-        for point2 in averages:
-            if point != point2:
-                distances.append(distance(point2, point))
-        while numpy.argmin(distances) in moves:
-            distances.remove(distances[numpy.argmin(distances)])
-        moves.append(numpy.argmin(distances))
-
-    order = []
-    order.append(0)
-    currentContour=0
-
-    for m in moves:
-        order.append(moves[currentContour])
-        currentContour = moves[currentContour]
-
-    print(order)
-
-def distance(p2, p1):
-    return math.sqrt((p2[0]-p1[0])**2 + (p2[1]-p1[1])**2)
+    cont_iter = ContourIterator(contours)
+    
 
 if __name__ == "__main__":
     main()
